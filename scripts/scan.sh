14431b63dcaa6fbaf66aad5acd662046c332bc70
daniel_git <d.k@blubb.de>
2026-09-04 13:15:23 +0200

    K: scan
    M: phone numbers

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,5 @@
-# TODO: write the script
+echo "\n======== SCANNING FOR CREDIT CARD NUMBERS ====="
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "========== CREDIT CARD SCANNING COMPLETE ========="
+echo "scurity numbers"
+echo "phone numbers"
